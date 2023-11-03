06ac9c31f35b1b596ef6710d7a29b9a001de7ad2
ironmanan <manan.shukla97@gmail.com>
2023-11-03 14:31:54 -0400

    with CAPTCHA

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const send_otp_routes = require("./otp_routes");
-const recover_pass_routes =require("./recover_pass_routes");
-const mysql = require('mysql2');
-const { encrypt, decrypt } = require('../util/encryptionUtil'); // Assuming the encryptionUtil.js file is in the same directory
-
+const recover_pass_routes = require("./recover_pass_routes");
+const axios = require('axios'); // Required for reCAPTCHA verification
 const db = require('../models');
-const { where } = require('sequelize');
-const EndUser = db.EndUsers;
-const User = db.User;
-const OTP = db.OTP;
+const { encrypt, decrypt } = require('../util/encryptionUtil');
+
+const multer = require('multer');
+const upload = multer();
 
 router.get("/Home_Landing", (req, res) => {
     console.log('Successfully in Root :Inssss:sss:: /');
@@ -19,7 +17,6 @@ router.get("/Home_Landing", (req, res) => {
 router.get('/login', (req, res) => {
     console.log('Successfully in Root :Inssss:sss:: /');
     res.render('loginPage');
-
 })
 
 router.get('/register', (req, res) => {
@@ -30,36 +27,63 @@ router.get('/forgot-password', (req, res, next) => {
     res.render('forgot-password');
 });
 
-router.post('/registerUser', async (req, res) => {
-    // Get data from request
-    const userData = req.body;
-    const User = db.User;
-    console.log(userData);
+router.post('/registerUser',upload.none(), async (req, res) => {
+    const recaptchaResponse = req.body['g-recaptcha-response'];
+    console.log("IN REGISTER USER NOW WILL PRINT REQ")
+    console.log(req)
+    // Check if CAPTCHA response is present
+    if (!recaptchaResponse) {
+        return res.status(400).json({ message: 'Please complete the CAPTCHA.BACKEND' });
+    }
 
     try {
-        // const user = await User.create(userData);
-        await User.create(userData).then((user) => {
-            userData.userId = user.userid;
-            console.log("User Inserted, now have to insert EndUser");
-            console.log(userData);
-            EndUser.create(userData);
-        });
-        // Handle the response after success
-        res.redirect('Home_Landing');  // Redirect to login or any other page
+        // Verify reCAPTCHA
+        const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=6Lffbu8oAAAAAHnr8NxZtRoP9-f7367MM9S_MjtN&response=${recaptchaResponse}`;
+        const verificationResponse = await axios.post(verificationURL);
+
+        if (!verificationResponse.data.success) {
+            return res.status(400).send('CAPTCHA verification failed.');
+        }
+
+        const userData = req.body;
+        const User = db.User;
+        console.log(userData);
+
+        // await User.create(userData).then((user) => {
+        //     userData.userId = user.userid;
+        //     console.log("User Inserted, now have to insert EndUser");
+        //     console.log(userData);
+        //     //EndUser.create(userData);
+        // });
+
+        return res.status(200).json({ message: 'Welcome ' + User.name + ',  Registration Successful' });
+
     } catch (error) {
-        // Handle the error response
         console.error('Error occurred:', error);
         res.status(500).send('Error occurred');
     }
 });
 
 router.post('/login', async (req, res) => {
-    // Get data from request
     const { emailId, password } = req.body;
-
     const User = db.User;
 
+    const recaptchaResponse = req.body['g-recaptcha-response'];
+    console.log(req)
+    // Check if CAPTCHA response is present
+    if (!recaptchaResponse) {
+        return res.status(400).json({ message: 'Please complete the CAPTCHA.' });
+    }
+
     try {
+        // Verify reCAPTCHA
+        const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=6Lffbu8oAAAAAHnr8NxZtRoP9-f7367MM9S_MjtN&response=${recaptchaResponse}`;
+        const verificationResponse = await axios.post(verificationURL);
+
+        if (!verificationResponse.data.success) {
+            return res.status(400).json({ message: 'CAPTCHA verification failed.' });
+        }
+
         const user = await User.findOne({ where: { emailId } });
 
         if (!user) {
@@ -69,10 +93,10 @@ router.post('/login', async (req, res) => {
         const decryptedPassword = decrypt(user.encrypted_password);
 
         if (password !== decryptedPassword) {
-            return res.status(401).json({ message: 'You have entered Invalid password, '+user.name+'' });
+            return res.status(401).json({ message: 'You have entered an Invalid password, ' + user.name });
         }
 
-        return res.status(200).json({ message: 'Welcome '+user.name+',  Login successful' });
+        return res.status(200).json({ message: 'Welcome ' + user.name + ',  Login successful' });
 
     } catch (error) {
         console.error('Error during login:', error);
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ OTP.belongsTo(User, {
 });
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
