02609235da1560c10270a96c006187a67f0f21ab
Kush Pandya <1705701@kiit.ac.in>
2023-11-19 20:08:25 -0500

    update

--- routes/cart_routes.js
+++ routes/cart_routes.js
@@ -290,6 +290,8 @@ router.get('/fetch-cart', async (req, res)=>
 {
     console.log("fetch-cart inside ------------------------")
 
+    console.log(req.cookies)
+
     //get authentication status and user id
     const [authentication, userId] = await authent(req,res);
 
@@ -312,7 +314,8 @@ router.get('/fetch-cart', async (req, res)=>
 
 
     //return cart details
-    res.status(200).send(cartDetails,{cartDetails});
+    res.status(200)
+    res.send(cartDetails);
 
 });
 
@@ -345,6 +348,7 @@ router.post('/add-payment-information', async (req, res)=>
     console.log("add-payment-information inside ------------------------")
 });
 
+
 router.get('/fetch-address', async (req, res)=>
 {
     console.log("fetch-address inside ------------------------")
@@ -362,12 +366,12 @@ router.post('/check-promo-code', async (req, res)=>
     //parse promoCode from request
     //parse query parameters
     console.log("req.query: ", req.body);
-    const promoCode = req.body.promoCode;
+    const promoCode1 = req.body.promocode;
 
-    console.log("promoCode: ", promoCode);
+    console.log("promoCode: ", promoCode1);
 
     //check if promoCode exists in db
-    const promoCodeData = await Promocode.findOne({where:{promocode:promoCode}});
+    const promoCodeData = await Promocode.findOne({where:{promocode:promoCode1}});
 
     if(!promoCodeData)
     {
@@ -392,6 +396,9 @@ router.post('/check-promo-code', async (req, res)=>
 
 });
 
+router.get('/orderplace', async (req, res) => { res.render('orderplace') });
+
+
 router.get('/get-final-cost', async (req, res)=>
 {
     console.log("get-final-cost inside ------------------------")
@@ -414,8 +421,6 @@ router.get('/get-final-cost', async (req, res)=>
     }
 
 
-    
-    
     promocode = -50
     sales = total_price*0.1
     finalPrice = total_price - promocode + sales
@@ -431,9 +436,30 @@ router.get('/get-final-cost', async (req, res)=>
         Sales:sales,
         FinalPrice:finalPrice
     });
-    res.render()
+//    res.render()
 
 
+});
+
+router.post('/checkout', async (req, res)=>
+{
+    console.log("checkout inside ------------------------")
+
+    //get authentication status and user id
+    const [authentication, userid] = await authent(req,res);
+    
+    if(!authentication)
+    {
+        return;
+    }
+
+    //get body from request
+    //parse query parameters
+    console.log("req.query: ", req.body);
+
+    //redirect to order confirmation page
+    res.render('orderplace')
+    res.status(200).send("Order placed successfully");
 });
 
 
