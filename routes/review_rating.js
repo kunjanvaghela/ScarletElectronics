0f43dcea90dfbf4bb0c15a5ac0c99e43b56ef647
MANAD DESAI <desaimanad123@gmail.com>
2023-11-20 12:24:47 -0500

    Added the review table schema and roting basics

---
+++ routes/review_rating.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require('../models');
+const { where } = require('sequelize');
+const Catalog = db.Catalog;
+const ItemListing = db.ItemListing;
+const UserUtil = require('../util/userUtil');
