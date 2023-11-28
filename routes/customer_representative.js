0d3bbbafc044153365454466b4c8fc28003151be
SanchayKanade <sanchay2598@gmail.com>
2023-11-28 00:40:55 -0500

    Customer Support half apis made from user and staff side

--- routes/customer_representative.js
+++ routes/customer_representative.js
@@ -4,15 +4,64 @@ const router = express.Router();
 const db = require('../models');
 const { where } = require('sequelize');
 const EndUserRequest = db.EndUserRequest;
-const ItemListing = db.ItemListing;
+const User = db.User;
+
 const UserUtil = require('../util/userUtil');
 
 router.get("/", async (req, res) => {
     console.log('Working');
     userDetails = await UserUtil.check_email(req.cookies.emailId);
+    console.log(userDetails);
     const username = userDetails.name;
+    const custRepId = userDetails.userid;
     console.log("Username is " + username);
-    res.render('customer_rep_dasboard', {username});
+
+    EndUserRequest.findAll({
+        where: {
+            customer_rep: custRepId
+        }
+      }).then((requests) => {
+        const serializedRequests = requests.map((request) => {
+          return {
+            requestId: request.requestId,
+            userId: request.userId,
+            listingId: request.listingId,
+            updateDescription: request.update_description,
+            createdOn: request.created_on,
+            currentStatus: request.current_status,
+            customerRep: username,
+            updatedn: request.updated_on
+          };
+        });
+    
+        // Send the serialized data as a response
+        res.render('customer_rep_dasboard', {serializedRequests, username});
+        // res.render('seller_listing', {serializedListings})
+      }).catch((error) => {
+        console.error('Error retrieving data:', error);
+        res.status(500).send('Internal server error');
+      });
+
+
+    //res.render('customer_rep_dasboard', {username});
+    
+});
+
+router.get("/show-all-request", async (req, res) => {
+    console.log('Button Working');
+    userDetails = await UserUtil.check_email(req.cookies.emailId);
+    console.log(userDetails.userid);
+    const username = userDetails.name;
+    const item_listing = EndUserRequest.findAll().then(function(Catalog){
+        
+        res.render('all-requests', {item_listing, username});
+        //console.log(Catalog);
+        
+      }).catch(function(err){
+        console.log('Oops! something went wrong, : ', err);
+      });
+    //console.log(item_listing);
+    //console.log(typeof(item_listing));
     
 });
 
