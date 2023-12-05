3a0d3c410cf6ce9a1421b624ef482bcc2f661292
Madhura Gajanan Angchekar <madhuraangchekar0@gmail.com>
2023-12-04 21:28:54 -0500

    delete and modify functionality

--- routes/customer_representative.js
+++ routes/customer_representative.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const db = require('../models');
 const { where } = require('sequelize');
 const EndUserRequest = db.EndUserRequest;
+const ItemListing = db.ItemListing; //newcode
 const User = db.User;
 
 const UserUtil = require('../util/userUtil');
@@ -150,6 +151,70 @@ router.get("/thread", async (req, res) => {
   res.render("threads.ejs",{ username });
 });
 
+//newcode
+router.get("/rep_listings", async (req, res) => {
+
+  try {
+    userDetails = await UserUtil.check_email(req.cookies.emailId);
+    console.log(userDetails.userid);
+    const username = userDetails.name;
+    const rep_listings = await ItemListing.findAll();
+    res.render('rep_listings', { rep_listings, username });
+  } catch (error) {
+    console.error('Error fetching listings:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Route to handle deletion of a listing
+router.post("/rep_listings/delete/:listingId", async (req, res) => {
+
+  const listingId = req.params.listingId;
+  try {
+    userDetails = await UserUtil.check_email(req.cookies.emailId);
+    console.log(userDetails.userid);
+    const username = userDetails.name;
+    await ItemListing.destroy({ where: { listingId } });
+    res.redirect('/customer_representative/rep_listings');
+  } catch (error) {
+    console.error('Error deleting listing:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Route to render the modification form
+router.get("/rep_listings/modify/:listingId", async (req, res) => {
+
+  const listingId = req.params.listingId;
+  try {
+    userDetails = await UserUtil.check_email(req.cookies.emailId);
+    console.log(userDetails.userid);
+    const username = userDetails.name;
+    const listing = await ItemListing.findByPk(listingId);
+    res.render('modify_listing', { listing, username });
+  } catch (error) {
+    console.error('Error fetching listing for modification:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Route to handle the form submission for modification
+router.post("/rep_listings/modify/:listingId", async (req, res) => {
+
+  const listingId = req.params.listingId;
+  try {
+    userDetails = await UserUtil.check_email(req.cookies.emailId);
+    console.log(userDetails.userid);
+    const username = userDetails.name;
+    // Update the ItemListing with the new values from the form
+    await ItemListing.update(req.body, { where: { listingId } });
+
+    res.redirect('/customer_representative/rep_listings');
+  } catch (error) {
+    console.error('Error modifying listing:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 module.exports = router;
 
 
