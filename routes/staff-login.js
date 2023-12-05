a7aac3114594771e273a4ff253eaa084efb3e58f
Madhura Gajanan Angchekar <madhuraangchekar0@gmail.com>
2023-12-05 10:56:23 -0500

    fixed staff login

--- routes/staff-login.js
+++ routes/staff-login.js
@@ -1,133 +1,77 @@
 const express = require("express");
 const router = express.Router();
-
 const mysql = require('mysql2');
-// Assuming the encryptionUtil.js file is in the same directory
-
 const db = require('../models');
 const { where } = require('sequelize');
-const User = db.User;
 const UserUtil = require('../util/userUtil');
 const { encrypt, decrypt } = require("../util/encryptionUtil");
 
-           
-// router.get("/login", async (req, res) => {
-// 	console.log("Successfully in Root :Inssss:sss:: /");
-// 	res.render("staffloginPage");
-// });
+const User = db.User;
+const Staff = db.Staff;
 
-const getlogin = async(req,res) => {
- 	console.log("Successfully in Root :Inssss:sss:: /");
- 	res.render("staffloginPage");
-};
+// GET route to render the login page
+router.get("/login", async (req, res) => {
+  res.render("staffloginPage");
+});
 
-const postlogin = async (req, res) => {
-	const { emailId, password } = req.body;
-	const User = db.User;
-	
-	console.log("IN login");
+// POST route to handle login
+router.post("/login", async (req, res) => {
+  const { emailId, password } = req.body;
 
-    try{
-		const user = await User.findOne({ where: { emailId } });
-		console.log(user.emailId);
-		if (!user) {
-			console.log("HEre");
-			return res.status(404).json({
-				success: false,
-				message: "You do not exist in our system.",
-			});
-		}
+  try {
+    // Check if the emailId exists in the users table
+    const user = await User.findOne({ where: { emailId } });
 
-		const decryptedPassword = decrypt(user.encrypted_password);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "You do not exist in our system.",
+      });
+    }
 
-		if (password !== decryptedPassword) {
-			return res.status(401).json({
-				success: false,
-				message: "You have entered an invalid password, " + user.name,
-			});
-		}
+    // Check if the userId exists in the staff table
+    const staffUser = await Staff.findOne({ where: { userId: user.userId } });
 
-		// Calculate the expiration time as the current time + 120 minutes
-		const tenMinutes = 1000 * 60 * 120; // 120 minutes in milliseconds
-		const expiresAt = new Date(Date.now() + tenMinutes);
+    if (!staffUser) {
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorized to log in as a staff member.",
+      });
+    }
 
-		// Set the cookie
-		res.cookie("emailId", user.emailId, {
-			expires: expiresAt,
-			httpOnly: false,
-		});
+    // Check if the provided password matches the hashed password in the database
+	const decryptedPassword = decrypt(user.encrypted_password);
 
-		// Send a JSON response indicating success and possibly a redirect URL.
-		return res.status(200).json({
-			success: true,
-			message: "Welcome " + user.emailId + ", Login successful",
-			// redirectUrl: "/customer_representative/",
-		});
-
-    } catch (error) {
-		console.error("Error during login:", error);
+	if (password !== decryptedPassword) {
 		return res.status(401).json({
 			success: false,
-			message: "Login Failed. Please use valid credentials.",
+			message: "You have entered an invalid password, " + user.name,
 		});
 	}
-};
 
-router.get("/login", getlogin);
-router.post("/login", postlogin);
+    // Calculate the expiration time as the current time + 120 minutes
+    const tenMinutes = 1000 * 60 * 120; // 120 minutes in milliseconds
+    const expiresAt = new Date(Date.now() + tenMinutes);
 
+    // Set the cookie
+    res.cookie("emailId", user.emailId, {
+      expires: expiresAt,
+      httpOnly: false,
+    });
 
+    // Send a JSON response indicating success and possibly a redirect URL.
+    return res.status(200).json({
+      success: true,
+      message: "Welcome " + user.emailId + ", Login successful",
+    });
 
-// router.post("/login", async (req, res) => {
-// 	const { emailId, password } = req.body;
-	
-// 	console.log("IN login");
-
-//     try{
-// 		const user = await User.findOne({ where: { emailId } });
-// 		console.log(user.emailId);
-// 		if (!user) {
-// 			console.log("HEre");
-// 			return res.status(404).json({
-// 				success: false,
-// 				message: "You do not exist in our system. Please Sign up",
-// 			});
-// 		}
-
-// 		const decryptedPassword = decrypt(user.encrypted_password);
-
-// 		if (password !== decryptedPassword) {
-// 			return res.status(401).json({
-// 				success: false,
-// 				message: "You have entered an invalid password, " + user.name,
-// 			});
-// 		}
-
-// 		// Calculate the expiration time as the current time + 120 minutes
-// 		const tenMinutes = 1000 * 60 * 120; // 120 minutes in milliseconds
-// 		const expiresAt = new Date(Date.now() + tenMinutes);
-
-// 		// Set the cookie
-// 		res.cookie("emailId", user.emailId, {
-// 			expires: expiresAt,
-// 			httpOnly: false,
-// 		});
-
-// 		// Send a JSON response indicating success and possibly a redirect URL.
-// 		return res.status(200).json({
-// 			success: true,
-// 			message: "Welcome " + user.emailId + ", Login successful",
-// 			redirectUrl: "/customer_representative/",
-// 		});
-
-//     } catch (error) {
-// 		console.error("Error during login:", error);
-// 		return res.status(401).json({
-// 			success: false,
-// 			message: "Login Failed. Please use valid credentials.",
-// 		});
-// 	}
-// });
-
+  } catch (error) {
+    console.error("Error during login:", error);
+    return res.status(401).json({
+      success: false,
+      message: "Login Failed. Please use valid credentials.",
+    });
+  }
+});
 
 module.exports = router;
