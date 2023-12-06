78448030321e6ff449acf5d6141ed323823cc72d
Madhura Gajanan Angchekar <madhuraangchekar0@gmail.com>
2023-12-06 13:30:32 -0500

    updated additem page

--- routes/add_item.js
+++ routes/add_item.js
@@ -15,45 +15,46 @@ router.get("/add", async (req, res) => {
     if (!UserUtil.authenticateToken(req.cookies.accessToken)) {
         // If not authenticated, send a 401 Unauthorized response
         return res.status(401).send('Authentication failed');
-      }
+    }
     const userDetails = await UserUtil.check_email(req.cookies.emailId);
     const username = userDetails.name;
     console.log('username : ',  username);
     res.render('additem', { username });
-    
 });
 
+// // Category selection route
+// router.post('/insert-category', async (req, res) => {
+//     const selectedCategory = req.body.category;
 
+//     // Render the appropriate form based on the selected category
+//     res.render('additem', { selectedCategory });
+// });
+
+// Insert data into the database based on the selected category
 router.post('/insert', async (req, res) => {
-
-    //Cookie-Presence check. TODO can be used further to validate user.
-    console.log(req.cookies);
-    
-    // Get data from request
     const catalogData = req.body;
-    const Catalog = db.Catalog;
-
     catalogData.listing_Status = 'Active'; // Example value
     catalogData.approval_Status = 'Approved'; // Example value
     catalogData.created_on = new Date();
     catalogData.remarks = 'No remarks'; // Example value
     catalogData.updated_on = new Date();
-
     console.log(catalogData);
-    
 
     try {
-        
+        // Get the user details using the emailId from the cookie
+        const userDetails = await UserUtil.check_email(req.cookies.emailId);
+        const userid = userDetails.userId;
+
+        // Add created_by to catalogData
+        catalogData.created_by = userid;
         const catalog = await Catalog.create(catalogData);
         // Handle the response after success
-        res.redirect('/item-listing');  // Redirect to login or any other page
+        res.redirect('/item-listing');  // Redirect to the item listing or any other page
     } catch (error) {
         // Handle the error response
         console.error('Error occurred:', error);
         res.status(500).send('Error occurred');
     }
 });
-
-
 
 module.exports = router;
